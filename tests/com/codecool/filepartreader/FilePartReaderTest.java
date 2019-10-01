551be8b93384268e09e4c0279cea886ac5ad9086
Barna Becze <barna.bcz@gmail.com>
2019-10-01 13:50:38 +0200

    Assert if setup throws exception

--- tests/com/codecool/filepartreader/FilePartReaderTest.java
+++ tests/com/codecool/filepartreader/FilePartReaderTest.java
@@ -1,7 +1,16 @@
 package com.codecool.filepartreader;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilePartReaderTest {
 
+    @Test
+    public void testIsIllegalArgumentExceptionThrown() {
+        FilePartReader fpr = new FilePartReader();
+        assertThrows(IllegalArgumentException.class, () -> {
+            fpr.setup(fpr.filePath, fpr.fromLine, fpr.toLine);
+        });
+    }
 }
