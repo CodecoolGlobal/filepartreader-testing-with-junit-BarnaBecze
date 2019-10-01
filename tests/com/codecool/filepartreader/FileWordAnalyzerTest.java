8613099ac7ac297bab843475241cdc22b3338a74
Barna Becze <barna.bcz@gmail.com>
2019-10-01 15:37:00 +0200

    Test alphabetical ordering

--- tests/com/codecool/filepartreader/FileWordAnalyzerTest.java
+++ tests/com/codecool/filepartreader/FileWordAnalyzerTest.java
@@ -1,7 +1,30 @@
 package com.codecool.filepartreader;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileWordAnalyzerTest {
 
+    @Test
+    public void testIsOrderedAlphabetically() throws IOException {
+        FilePartReader fpr = new FilePartReader();
+        fpr.setup("docs/simpleText.txt", 1, 1);
+        FileWordAnalyzer fwa = new FileWordAnalyzer(fpr);
+        List<String> expected = new ArrayList<String>() {{
+            add("a");
+            add("is");
+            add("simple");
+            add("test");
+            add("This");
+        }};
+        String words = fwa.fpr.readLines();
+        System.out.println(words);
+        assertEquals(expected, fwa.getWordsOrderedAlphabetically(words.split("\\W")));
+    }
 }
