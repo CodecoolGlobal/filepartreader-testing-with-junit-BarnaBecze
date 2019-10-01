5282d45fa2bf4b6045b5a28a6add4f42263a200f
Barna Becze <barna.bcz@gmail.com>
2019-10-01 16:27:32 +0200

    Refactor (minor)

--- src/com/codecool/filepartreader/FileWordAnalyzer.java
+++ src/com/codecool/filepartreader/FileWordAnalyzer.java
@@ -3,7 +3,6 @@ package com.codecool.filepartreader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class FileWordAnalyzer {
